a638a3ad59f7635de1eee54bc8de61c1a5739b6c
syahirain <syahiranahmad@gmail.com>
2023-03-30 02:14:10 +0800

    update md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# subs-platform
-test
+# Subscription Platform
+
+## Command
+- php artisan make:command sendEmails 
+
+## EMAIL CONFIG
+![Alt text](/storage/images/email_config.png?raw=true "Email Config")
+
+## WEBSITE SQL FILE
+
+image.png
---
+++ website.sql
@@ -0,0 +1,4 @@
+INSERT INTO `website` (`id`, `domain`) VALUES (1, 'facebook.com');
+INSERT INTO `website` (`id`, `domain`) VALUES (2, 'github.com');
+INSERT INTO `website` (`id`, `domain`) VALUES (3, 'google.com');
+INSERT INTO `website` (`id`, `domain`) VALUES (4, 'inisev.com');
